f7bb74f0ef042b226631187f9323f852963b94ca
Maxence Frenette <maxence.frenette@gmail.com>
2020-04-18 17:12:16 -0700

    Make website viewable offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,9 +61,7 @@ module.exports = {
                 icon: `content/images/gatsby-icon.png`,
             },
         },
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
+        `gatsby-plugin-offline`,
         {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
